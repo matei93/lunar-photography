09c2ceee2d87fdac50901c72ef9ca5d544bc866a
matei93 <97781920+matei93@users.noreply.github.com>
2024-11-23 12:21:22 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBYLZRnX4CNFbwQgWB1lSYe_taUE0angKU",
